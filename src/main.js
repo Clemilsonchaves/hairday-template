e0a7cba08567d42ba5ce7b2ab3aed3e96e1e15f7
clemilson chaves <cle_milson_silva@hotmail.com>
2024-10-23 04:47:32 -0300

    carregando a data atual

--- src/main.js
+++ src/main.js
@@ -8,5 +8,4 @@ import "./styles/global.css"
 import "./styles/form.css"
 import "./styles/schedule.css"
 
-import dayjs from "dayjs"
-console.log(dayjs().format("DD/MM - HH:mm")) 
+import "./modules/form/submit.js"
---
+++ src/modules/form/submit.js
@@ -0,0 +1,19 @@
+import dayjs from "dayjs"
+
+const form = document.querySelector("form") 
+const selectedDate = document.getElementById("date")
+
+// Data atual para formatar o input
+const inputToday = dayjs(new Date()).format("YYYY-MM-DD")
+
+// Carregar a data atual e define a data minima como sendo a data atual  
+selectedDate.value = inputToday
+selectedDate.min = inputToday
+
+form.onsubmit =(event) => {
+  // Previne o comportamento padão de carregar a página
+  event.preventDefault()
+
+  console.log("Enviado!")
+}
+  
