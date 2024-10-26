7c12b2ec550e84ca29abbb029af861914fac2d59
clemilson chaves <cle_milson_silva@hotmail.com>
2024-10-26 05:46:58 -0300

    Configurando a página de agendamento

---
+++ src/modules/form/hours-load.js
@@ -0,0 +1,8 @@
+import { openingHours } from "../../utils/opening-hours.js"
+export function hoursLoad(){
+const opening = openingHours.map((hour) => {
+  const [scheduleHour] = hour.split(":")
+  console.log((scheduleHour)) 
+  
+})
+}
